ac815dff2b9262ea1376b0e84f3e97a79e15923c
Miguel Ferreira <miguelferreira@me.com>
2021-06-08 10:42:15 +0200

    Fix devex-cli description

--- Formula/devex-cli.rb
+++ Formula/devex-cli.rb
@@ -1,5 +1,5 @@
 class DevexCli < Formula
-  desc "DevEx cli - automating development gruntwork"
+  desc "Automating development gruntwork"
   homepage "https://miguelaferreira.gitbook.io/devex/devex-cli/overview"
   url "https://github.com/miguelaferreira/devex-cli/releases/download/v0.1.0/all-files-v0.1.0.tar.gz"
   sha256 "494da279a2500b49eb3b3c5cecbea70a54f2e3c0ced3c17df9baeaf6c018e2c5"
