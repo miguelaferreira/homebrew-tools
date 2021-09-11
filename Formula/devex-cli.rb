620bccd9d8b8292020707babe6c4572485d33296
Miguel Ferreira <miguelferreira@me.com>
2021-09-11 11:43:49 +0200

    [devex-cli] Upgrade to v1.0.0

--- Formula/devex-cli.rb
+++ Formula/devex-cli.rb
@@ -1,8 +1,8 @@
 class DevexCli < Formula
   desc "Automating development gruntwork"
   homepage "https://miguelaferreira.gitbook.io/devex/devex-cli/overview"
-  url "https://github.com/miguelaferreira/devex-cli/releases/download/v0.2.4/all-files-v0.2.4.tar.gz"
-  sha256 "34c6bb3da5660899ed085f74e9206ee49af6e9cbf09c95298a066d2f4b35cef7"
+  url "https://github.com/miguelaferreira/devex-cli/releases/download/v1.0.0/all-files-v1.0.0.tar.gz"
+  sha256 "c9cb100102870d05ddad6a27d06907d1c04bdd881570ee85523cf4086ce1c3da"
   license "MIT-Modern-Variant"
 
   bottle :unneeded
