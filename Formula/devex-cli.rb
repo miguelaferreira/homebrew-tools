f8c025dd08741d312f6a6dafcf575014dc0ac2a6
Miguel Ferreira <miguelferreira@me.com>
2021-11-06 19:36:30 +0100

    Add Java 17 warning to devex-cli formula

--- Formula/devex-cli.rb
+++ Formula/devex-cli.rb
@@ -1,7 +1,7 @@
 class DevexCli < Formula
   desc "Automating development gruntwork"
   homepage "https://miguelaferreira.gitbook.io/devex/devex-cli/overview"
-  url "https://github.com/miguelaferreira/devex-cli/archive/v1.2.5.tar.gz"
+  url "https://github.com/miguelaferreira/devex-cli/archive/refs/tags/v1.2.5.tar.gz"
   sha256 "5a703c0aa3ceb3c3ad8c67b5c7ee0b162feb297b34a2c536718910d0f3a4a72c"
   license "MIT-Modern-Variant"
 
@@ -24,6 +24,8 @@ class DevexCli < Formula
     bin.write_jar_script libexec/"lib/devex-#{version}-all.jar", "devex"
 
     ohai "🤓 Check the toolkit docs 📘 out at https://miguelaferreira.gitbook.io/devex/devex-cli/overview"
+    ohai "⚠️ devex requires Java 17 to execute"
+    ohai "Please make sure an older version of Java isn't configured via JAVA_HOME ⚠️"
   end
 
   test do
